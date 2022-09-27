78dd561371d1c49ce7c8bdc9e80601af52473c4f
Viktor Johansson <viktor1.johansson.ext@lkab.com>
2022-09-27 13:42:56 +0200

    OOP DOP Init

---
+++ OOPnDOP/OOPnDOP/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+namespace DevCommunity
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+
+    }
+
+}
